d7dc8384109e6948db62fe0d202957bda62b1134
Synacten <blackhorns9999@gmail.com>
2019-08-01 17:23:40 +0300

    search query

--- controllers/search.js
+++ controllers/search.js
@@ -2,7 +2,7 @@ const { db } = require('../database/connect');
 
 const searchArticle = async (req, res) => {
   const { searchData } = req.body;
-  const { rows } = await db.query(`SELECT * FROM article WHERE title LIKE '%${searchData}%'`);
+  const { rows } = await db.query(`SELECT * FROM article WHERE title LIKE '%${searchData}%' OR description LIKE '%${searchData}%' LIMIT 10`);
   res.status(200).json({ rows });
 };
 
